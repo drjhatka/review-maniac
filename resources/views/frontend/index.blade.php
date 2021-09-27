ac410e63dff787a03465e32e5f64dd7105f25d44
drjhatka <bishawjit12@gmail.com>
2021-09-27 18:51:36 +0600

    search interface partially done

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -5,20 +5,33 @@
 @section('content')
  @include('partials.latest-slider')
 
- <div class="col-md-12 card bg-primary">
-     <div class="card-header search">
-       Search Reviews
-     </div>
-
+    <div class="col-md-12 card bg-primary">
+        <div class="card-header search">
+          Search Reviews
+        </div>
     </div>
+
     <div class="row">
-      {!! Form::open() !!}
-        <div class="col-md-5">
-            {!! Form::label('search-filter', 'Search Filters', []) !!}
+      {!! Form::open(['class'=>'row mt-2']) !!}
+        <div class=" offset-1 col-md-2">
+            {!! Form::label('search-filter', 'Search Filters', ['class'=>' search-label']) !!}
 
         </div>
-        <div class="card col-md-5">
-          {!! Form::select('search-filter', ['pstars'=>'PrnStar', 'movies'=>'Movies','channel='=>'Channel'], true,[]) !!}
+        <div class=" col-md-2">
+          {!! Form::select('search-filter', ['all'=>'All','pornstar'=>'Pornstar', 'movie'=>'Movies','channel='=>'Channel'], true,
+          ['class'=>'form-control']) !!}
+        </div>
+
+        <div class=" col-md-3">
+          
+          {!! Form::text('search-text', null, ['class'=>'form-control','placeholder'=>'Write pornstars name here ']) !!}
+          
+        </div>
+
+        <div class=" col-md-2 offset-1">
+           
+           {!! Form::submit('Search', ['class'=> 'form-control btn btn-primary']) !!}
+           
         </div>
 
         {!! Form::close() !!}
